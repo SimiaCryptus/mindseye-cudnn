96f449ef32b290d3b658750622a963ac2f4f4a11
Andrew Charneski <acharneski@gmail.com>
2021-01-01 18:15:36 -0800

    Fixed preview image

--- src/main/java/com/simiacryptus/mindseye/lang/cudnn/CudaSettings.java
+++ src/main/java/com/simiacryptus/mindseye/lang/cudnn/CudaSettings.java
@@ -158,9 +158,9 @@ public class CudaSettings implements Settings {
     if (appSettings.containsKey("worker.index") && !System.getProperties().containsKey("CUDA_DEVICES")) {
       System.setProperty("CUDA_DEVICES", appSettings.get("worker.index"));
     }
-    defaultDevices = get("CUDA_DEVICES", "1,2");
+//    defaultDevices = get("CUDA_DEVICES", "1,2");
 //    defaultDevices = get("CUDA_DEVICES", "0");
-//    defaultDevices = get("CUDA_DEVICES", "");
+    defaultDevices = get("CUDA_DEVICES", "");
   }
 
   /**
